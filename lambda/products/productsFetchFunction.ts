4237ac4e155f880c5a336eeb19659545d1b8203d
Matheus Sena <senamatheusjob@gmail.com>
2023-03-26 20:42:45 -0300

    FEAT: logs in lambda function

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -2,6 +2,10 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
     const method = event.httpMethod;
+    const lambdaRequestId = context.awsRequestId;
+    const apiRequestId = event.requestContext.requestId;
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - LambdaRequestId: ${lambdaRequestId}`)
     if (event.resource === "/products") {
         if (method === 'GET') {
             console.log('GET')
